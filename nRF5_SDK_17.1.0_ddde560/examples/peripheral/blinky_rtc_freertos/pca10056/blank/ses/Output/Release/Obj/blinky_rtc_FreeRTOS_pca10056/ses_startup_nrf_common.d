851383dcc29e35010d5dfaa0e316754dc4ca7988
park <parkjh@etaelec.com>
2024-10-01 21:55:09 +0900

    no message

---
+++ nRF5_SDK_17.1.0_ddde560/examples/peripheral/blinky_rtc_freertos/pca10056/blank/ses/Output/Release/Obj/blinky_rtc_FreeRTOS_pca10056/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/blinky_rtc_FreeRTOS_pca10056/ses_startup_nrf_common.o: \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
---
+++ nRF5_SDK_17.1.0_ddde560/examples/peripheral/blinky_rtc_freertos/pca10056/blank/ses/Output/Release/Obj/blinky_rtc_FreeRTOS_pca10056/system_nrf52840.d
@@ -0,0 +1,26 @@
+Output/Release/Obj/blinky_rtc_FreeRTOS_pca10056/system_nrf52840.o: \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52840.c \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52.c \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdbool.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52840.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52840_bitfields.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf51_to_nrf52840.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52_to_nrf52840.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\compiler_abstraction.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf_peripherals.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52840_peripherals.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf_erratas.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf51_erratas.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf52_erratas.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf53_erratas.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\nrf91_erratas.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52.h \
+ D:\nrf5-SDK\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\system_nrf52_approtect.h
